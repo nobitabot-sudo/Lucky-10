1f736f9f032ceda31427c4db34bb65b4bcaf275a
nobitabot-sudo <skillbuddyxs@gmail.com>
2025-07-31 01:28:24 +0530

    Update User.js

---
+++ models/User.js
@@ -0,0 +1,9 @@
+// models/userModel.js
+const { DataTypes } = require('@supabase/supabase-js');
+
+// Ye sirf placeholder hai, tum actual Supabase queries likhoge
+// yaha database ka schema define karne ki zarurat nahi (Supabase me hota hai)
+
+module.exports = {
+    table: 'users'
+};
