e0005749ce886c2dac73cf6da45755281fa5c9d2
nobitabot-sudo <skillbuddyxs@gmail.com>
2025-08-02 10:04:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
+const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
-const walletRoutes = require("./routes/walletRoutes");
-const betRoutes = require("./routes/betRoutes");
-const adminRoutes = require("./routes/adminRoutes");
 
+dotenv.config();
 const app = express();
-app.use(cors());
+
 app.use(express.json());
-
 app.use("/api/users", userRoutes);
-app.use("/api/wallet", walletRoutes);
-app.use("/api/bets", betRoutes);
-app.use("/api/admin", adminRoutes);
 
-app.get("/", (req, res) => res.send("LUCKY 10 Backend Running 🚀"));
+app.get("/", (req, res) => {
+    res.send("Lucky-10 API is running...");
+});
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
