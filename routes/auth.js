18d524c0d0ffe684ac2e0cb8f4077f2c16a831b1
nobitabot-sudo <skillbuddyxs@gmail.com>
2025-08-03 21:37:11 +0530

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// This file can be used for additional auth routes if needed
+// Currently, auth routes are in users.js
+
+module.exports = router;
