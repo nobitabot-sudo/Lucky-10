d5887830033e1bc5b4d3b69f32c56be2aa4f12b9
nobitabot-sudo <skillbuddyxs@gmail.com>
2025-08-03 21:29:34 +0530

    Delete config/db.js

--- config/db.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-require("dotenv").config();
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-const supabaseAdmin = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
-
-module.exports = { supabase, supabaseAdmin };
